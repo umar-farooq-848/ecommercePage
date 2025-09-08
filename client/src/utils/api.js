465e10752b8c133d4e5ff8d7a7d8bee98b5517f6
umarfarooqshaik645 <umarfarooqshaik645@gmail.com>
2025-09-08 14:08:33 +0000

    feat: Use environment variable for API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@
 // ========================================
 import axios from 'axios'
 
-const API_BASE = '/api'
+const API_BASE = import.meta.env.VITE_API_BASE || "/api";
 
 const api = axios.create({
   baseURL: API_BASE,
